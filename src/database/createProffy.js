39bfb759cf9297cff54a52b75a1ef0ce9231c700
victorvcth <victor_buzetti@hotmail.com>
2022-04-06 22:43:30 -0300

    first commit

---
+++ src/database/createProffy.js
@@ -0,0 +1,54 @@
+module.exports = async function(db, { proffyValue, classValue, classScheduleValues }) {
+    // inserir dados na tabela de proffys
+    const isertedProffy = await db.run(`
+            INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            "${proffyValue.name}",
+            "${proffyValue.avatar}",
+            "${proffyValue.whatsapp}",
+            "${proffyValue.bio}"            
+        );
+    `)
+
+    const proffy_id = isertedProffy.lastID
+
+    // inserir dados na tabela classes
+    const insertedClass = await db.run(`
+            INSERT INTO classes (
+                subject,
+                cost,
+                proffy_id
+            ) VALUES (
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
+            );
+    `)
+     const class_id = insertedClass.lastID
+
+    //  inserir dados na tabela class_schedule
+    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+        return db.run(`
+        INSERT INTO class_schedule (
+            class_id,
+            weekday,
+            time_from,
+            time_to
+        ) VALUES (
+            "${class_id}",
+            "${classScheduleValue.weekday}",
+            "${classScheduleValue.time_from}",
+            "${classScheduleValue.time_to}"
+        );
+        `)
+    })
+
+    // aqui vou excetar todos os db.runs() das class_schudules
+    await Promise.all(insertedAllClassScheduleValues)
+
+
+}
